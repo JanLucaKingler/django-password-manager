57f302e396ff1e2cf64f5f72d716161b56d04ded
JanLucaKingler <you@example.com>
2025-01-12 12:28:29 +0100

    CSS angepasst und URl erweitert

---
+++ mydjango/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def mydjango(request):
+    return render(request, 'django.html')
+
+
+def SignUpPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        password = request.POST.get('password')
+
+
+        my_user = User.objects.create_user(uname,password)
+        my_user.save()
+        return redirect('login')
+
+
+    return render(request, 'signup.html')
+
+
+def LoginPage(request):
+    if  request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+    return render(request, 'login.html')
